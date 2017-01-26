9906ce2747896fafda60a00b25254220cf4a237a
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-01-26 17:09:58 +0000

    Completed chapter 6 of TWD, all things appear to be working, also shifted some files around

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,11 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'tango_with_django_project/templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-STATIC_DIR = os.path.join(BASE_DIR, 'tango_with_django_project/static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
-MEDIA_DIR = os.path.join(BASE_DIR, 'tango_with_django_project/media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
